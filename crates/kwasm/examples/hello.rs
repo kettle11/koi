9364b35027984df846fd666e69661796afd6696d
Ian Kettlewell <ian.kettlewell@gmail.com>
2021-09-09 12:17:52 -0400

    Fix some warnings / errors

--- crates/kwasm/examples/hello.rs
+++ crates/kwasm/examples/hello.rs
@@ -5,11 +5,10 @@ fn main() {
     setup_panic_hook();
     console::log("HELLO WORLD!");
 
-    let console = JS_SELF.get_property("console");
-    let log_function = console.get_property("log");
+    let log_function = JS_SELF.get_property("console.log");
 
     let message = JSString::new("HI WORLD!!!");
-    log_function.call_1_arg(&console, &message);
+    log_function.call_1_arg(&message);
 
     console::log("LOGGING FROM THE CONSOLE");
 
