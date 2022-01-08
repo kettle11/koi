3194bb0be0a5f087a9984f49ffd4db224b33af7e
Ian Kettlewell <ian.kettlewell@gmail.com>
2022-01-08 16:01:40 -0500

    Update build script to specify nightly

--- web_build.sh
+++ web_build.sh
@@ -1,4 +1,4 @@
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals -Clink-arg=--max-memory=4294967296' \
-  cargo build --target wasm32-unknown-unknown -Z build-std=std,panic_abort --example $1 ${@:2} --release
+  cargo +nightly build --target wasm32-unknown-unknown -Z build-std=std,panic_abort --example $1 ${@:2} --release
 cp target/wasm32-unknown-unknown/release/examples/$1.wasm web_build/wasm.wasm
 cp crates/kwasm/js/kwasm.js web_build/kwasm.js
