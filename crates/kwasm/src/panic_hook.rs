dfc4409683fbe0f44afb4130edd5d86fde49f7c4
Ian Kettlewell <ian.kettlewell@gmail.com>
2022-02-17 23:03:59 -0500

    Add crash message and refresh page after crash

--- crates/kwasm/src/panic_hook.rs
+++ crates/kwasm/src/panic_hook.rs
@@ -3,6 +3,9 @@ use std::panic;
 fn hook_impl(info: &panic::PanicInfo) {
     let message = info.to_string();
     crate::libraries::console::error(&message);
+    crate::libraries::eval(
+        "alert(\"The application has crashed. If you'd like to report this error check the console for errors to send to the developer.      When this dialog is closed the page will refresh. \"); location.reload();",
+    );
 }
 
 /// Sets up a panic hook to print a slightly more useful error-message to the console.
