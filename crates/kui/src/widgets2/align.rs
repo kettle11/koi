b017280776390dc3f2145d804d469ebc9fcc7267
Ian Kettlewell <ian.kettlewell@gmail.com>
2022-02-22 23:53:58 -0500

    Generic align

--- crates/kui/src/widgets2/align.rs
+++ crates/kui/src/widgets2/align.rs
@@ -1,24 +1,42 @@
 use crate::*;
 
+pub enum Alignment {
+    Start,
+    End,
+}
 // In the future this should be generalized.
-pub fn align_horizontal_end_vertical_start<Data, Context>(
+pub fn align<Data, Context>(
+    horizontal: Alignment,
+    vertical: Alignment,
     child: impl Widget<Data, Context>,
 ) -> impl Widget<Data, Context> {
-    AlignUpperEnd {
+    Align {
         child,
         child_size: Vec3::ZERO,
+        align_direction: Vec3::new(
+            match horizontal {
+                Alignment::Start => 0.0,
+                Alignment::End => 1.0,
+            },
+            match vertical {
+                Alignment::Start => 0.0,
+                Alignment::End => 1.0,
+            },
+            0.0,
+        ),
         phantom: std::marker::PhantomData,
     }
 }
 
-struct AlignUpperEnd<Data, Context, Child: Widget<Data, Context>> {
+struct Align<Data, Context, Child: Widget<Data, Context>> {
     child: Child,
     child_size: Vec3,
+    align_direction: Vec3,
     phantom: std::marker::PhantomData<fn() -> (Data, Context)>,
 }
 
 impl<Data, Context, Child: Widget<Data, Context>> Widget<Data, Context>
-    for AlignUpperEnd<Data, Context, Child>
+    for Align<Data, Context, Child>
 {
     fn layout(
         &mut self,
@@ -35,12 +53,32 @@ impl<Data, Context, Child: Widget<Data, Context>> Widget<Data, Context>
         state: &mut Data,
         context: &mut Context,
         drawer: &mut Drawer,
-        mut constraints: Box3,
+        constraints: Box3,
     ) {
-        let corner = Vec3::new(constraints.max.x, constraints.min.y, constraints.min.z);
-        constraints.min = Vec3::new(corner.x - self.child_size.x, corner.y, corner.z);
-        constraints.max = constraints.min + self.child_size;
-        self.child.draw(state, context, drawer, constraints)
+        let inverse = Vec3::new(
+            if self.align_direction.x == 0.0 {
+                1.0
+            } else {
+                0.0
+            },
+            if self.align_direction.y == 0.0 {
+                1.0
+            } else {
+                0.0
+            },
+            if self.align_direction.z == 0.0 {
+                1.0
+            } else {
+                0.0
+            },
+        );
+        let min = constraints.min.mul_by_component(inverse)
+            + constraints.max.mul_by_component(self.align_direction)
+            - self.child_size.mul_by_component(self.align_direction);
+
+        let max = min + self.child_size;
+
+        self.child.draw(state, context, drawer, Box3 { min, max })
     }
     fn update(&mut self, state: &mut Data, context: &mut Context) {
         self.child.update(state, context)
