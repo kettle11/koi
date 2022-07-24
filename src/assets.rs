fbebdb40af50ce529ccf265330e48887c933e773
Ian Kettlewell <ian.kettlewell@gmail.com>
2022-07-24 15:54:01 -0400

    Add feature that enables loading assets from the internet

--- src/assets.rs
+++ src/assets.rs
@@ -438,6 +438,23 @@ unsafe impl<T> Sync for SyncGuard<T> {}
 
 #[cfg(not(target_arch = "wasm32"))]
 pub async fn fetch_bytes(path: &str) -> Result<Vec<u8>, ()> {
+    #[cfg(feature = "network_requests")]
+    let contents = if path.starts_with("http") {
+        use std::io::Read;
+
+        let response = ureq::get(path).call().map_err(|_| ())?;
+
+        let mut bytes: Vec<u8> = Vec::new();
+        response
+            .into_reader()
+            .take(10_000_000)
+            .read_to_end(&mut bytes)
+            .map_err(|_| ())?;
+        bytes
+    } else {
+        std::fs::read(path).unwrap_or_else(|_| panic!("No such path: {:?}", path))
+    };
+    #[cfg(not(feature = "network_requests"))]
     let contents = std::fs::read(path).unwrap_or_else(|_| panic!("No such path: {:?}", path));
     Ok(contents)
 }
